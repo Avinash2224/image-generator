664c9939b8ec9b419284f13c42111e28710a3add
Avinash <avinashsrivastav196@gmail.com>
2025-04-05 13:05:27 +0530

    new

--- server/index.js
+++ server/index.js
@@ -8,14 +8,17 @@ import posts from "./routes/Posts.js";
 dotenv.config();
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.urlencoded({ extended: true })); // For form data
 
+// Routes
 app.use("/api/generateImage/", generateImageRoute);
 app.use("/api/post/", posts);
 
-// error handler
+// Error Handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Something went wrong";
@@ -26,23 +29,26 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Test Route
 app.get("/", async (req, res) => {
   res.status(200).json({
     message: "Hello developers from GFG",
   });
 });
 
+// MongoDB Connection
 const connectDB = () => {
   mongoose.set("strictQuery", true);
   mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => console.log("Connected to Mongo DB"))
     .catch((err) => {
-      console.error("failed to connect with mongo");
+      console.error("Failed to connect with MongoDB");
       console.error(err);
     });
 };
 
+// Start the Server
 const startServer = async () => {
   try {
     connectDB();
